dd334e188eaf0afc41aad6ac727bd93521f3d0c1
Jair <jairbgarcia19@gmail.com>
2024-04-27 17:01:40 -0500

    feat: :sparkles: Introduccion de la clase

---
+++ Dia 11/modulo/script.js
@@ -0,0 +1,10 @@
+class pokemones{
+    constructor(){
+        this.elementos = {
+            nombrepokemon: document.querySelector('.nombre_pokemon'),
+            numeropokemon: document.querySelector('.numero_pokemon'),
+            imagenpokemon: document.querySelector('.poke_imagen'),
+            formulario: document.querySelector('.form')
+        }
+    }
+}
